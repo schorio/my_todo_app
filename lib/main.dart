dde106693c85af7cb9b6b1c6d624aacdd32fb085
schorio <schorioignace@gmail.com>
2022-11-14 15:06:29 +0300

    App floatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,22 @@ class MonPage extends StatefulWidget {
 
 class _MonPageState extends State<MonPage> {
 
+  String value='';
+  int val = 0;
+
+  void onShow() {
+    setState(() {
+      value = new DateTime.now().toString();
+      val+=1;
+    });
+  }
+  void offShow() {
+    setState(() {
+      val-=1;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,11 +49,29 @@ class _MonPageState extends State<MonPage> {
         title: Text('Test des bouttons'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisAlignment: MainAxisAlignment.spaceAround ,
         children: <Widget>[
-          
+          Text(
+            'value = $val , date = $value',
+            style: TextStyle(fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+          IconButton(
+            icon: Icon(Icons.account_balance_wallet_sharp, color: Colors.red,),
+            onPressed: offShow,
+            tooltip: "Moins",
+          ),
         ],
       ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: onShow,
+        backgroundColor: Colors.blue,
+        child: Icon(Icons.account_balance_wallet_sharp),
+      ),
+
+      
+
     );
   }
 }
