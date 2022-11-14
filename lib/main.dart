2455509c2177934e20b46537fea8c288757ce08d
schorio <schorioignace@gmail.com>
2022-11-14 19:27:17 +0300

    TextField avec CheckBox

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Premier test',
-      theme: ThemeData(
-        primarySwatch: Colors.cyan,
-      ),
-      home: MonPage()
-    );
+        title: 'Premier test',
+        theme: ThemeData(
+          primarySwatch: Colors.cyan,
+        ),
+        home: MonPage());
   }
 }
 
@@ -25,8 +24,8 @@ class MonPage extends StatefulWidget {
 }
 
 class _MonPageState extends State<MonPage> {
-
-  int radioVal=0;
+  int radioVal = 0;
+  String valeur = "";
 
   @override
   Widget build(BuildContext context) {
@@ -36,28 +35,41 @@ class _MonPageState extends State<MonPage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Text('Bienvenue'),
+            Text('Bienvenue $valeur $radioVal'),
+
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Nom',
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.blue,
+                  size: 25,
+                )
+              ),
+              onChanged: (value) {
+                setState(() {
+                  valeur = value;
+                });
+              },
+            ),
 
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [0,1,2,3,4,5]
-                .map((int index) => Radio<int>(
-                  value: index,
-                  groupValue: radioVal,
-                  onChanged: (int? value) {
-                    setState(() => radioVal = value!);
-                  },
-                ))
-                .toList(),         
+              children: [0, 1, 2, 3, 4, 5]
+                  .map((int index) => Radio<int>(
+                        value: index,
+                        groupValue: radioVal,
+                        onChanged: (int? value) {
+                          setState(() => radioVal = value!);
+                        },
+                      ))
+                  .toList(),
             )
           ],
         ),
-        
       ),
-      
-
     );
   }
 }
