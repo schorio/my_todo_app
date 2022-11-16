50b867c9e22828dd86f55a1f49c7c775fc4295d3
schorio <schorioignace@gmail.com>
2022-11-16 23:01:35 +0300

    ajout container dans "home.dart"

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -17,8 +17,16 @@ class Home extends StatelessWidget {
               color: tdBlack,
               size: 30,
             ),
+            Container(
+              height: 40,
+              width: 40,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset('assets/images/scho.jpg'),
+              ),
+            ),
           ]
-        )
+        ),
       ),
       
       body: Container(
