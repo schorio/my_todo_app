db3c9d7535918a7cb288a04d3568d9d2d6e9e607
schorio <schorioignace@gmail.com>
2022-11-16 21:23:12 +0300

    Update main du app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_todo_app/screens/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,66 +12,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-        title: 'Premier test',
+        title: 'My ToDo',
         theme: ThemeData(
           primarySwatch: Colors.cyan,
         ),
-        home: MonPage());
+        home: Home());
   }
 }
 
-class MonPage extends StatefulWidget {
-  @override
-  _MonPageState createState() => _MonPageState();
-}
-
-class _MonPageState extends State<MonPage> {
-  int radioVal = 0;
-  String valeur = "";
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Test des bouttons'),
-      // ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Text('Bienvenue $valeur $radioVal'),
-
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Nom',
-                icon: Icon(
-                  Icons.person,
-                  color: Colors.blue,
-                  size: 25,
-                )
-              ),
-              onChanged: (value) {
-                setState(() {
-                  valeur = value;
-                });
-              },
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [0, 1, 2, 3, 4, 5]
-                  .map((int index) => Radio<int>(
-                        value: index,
-                        groupValue: radioVal,
-                        onChanged: (int? value) {
-                          setState(() => radioVal = value!);
-                        },
-                      ))
-                  .toList(),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
