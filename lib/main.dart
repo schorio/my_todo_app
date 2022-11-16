5b90e6f7aa53aeb120ea0f0f9db2f3d242e78340
schorio <schorioignace@gmail.com>
2022-11-16 23:10:47 +0300

    Mettre en transparent le haut d'AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent)
+    );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
         title: 'My ToDo',
