896140d211d1509c2675dfdcbf5307ee42f22c85
schorio <schorioignace@gmail.com>
2022-11-17 21:43:49 +0300

    Modification de l'espacement entre les taches

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -10,7 +10,7 @@ class ToDoItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(bottom: 15),
+      margin: EdgeInsets.only(bottom: 12),
         child: ListTile(
           onTap: () {
             print('Test de clique du tache');
