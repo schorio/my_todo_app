fe82d501e26ebe0f4f20fa1f92cc380bddaca9ca
schorio <schorioignace@gmail.com>
2022-11-17 20:52:56 +0300

    Creation du container des taches

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -8,6 +8,14 @@ class ToDoItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('ToDo taches'),);
+    return Container(
+        child: ListTile(
+          onTap: () { },
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20)
+          ),
+          tileColor: Colors.white,
+        ) 
+      );
   }
 }
