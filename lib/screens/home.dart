3803be0d1ce960153941b7f64513e4c84c504bf5
schorio <schorioignace@gmail.com>
2022-11-18 22:48:22 +0300

    Personalisation du container de text des nouveaux taches

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -55,7 +55,29 @@ class Home extends StatelessWidget {
             alignment: Alignment.bottomCenter,
             child: Row(
               children: [
-                
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(
+                      bottom: 20,
+                      right: 20,
+                      left: 20
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 5,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0.0, 0.0),
+                        blurRadius: 10.0,
+                        spreadRadius: 0.0,
+                      )],
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+                  ),
+                )
               ],
             ),
           )
