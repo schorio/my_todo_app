8a62435108840637628486868d1ca8110c84d526
schorio <schorioignace@gmail.com>
2022-11-18 23:50:01 +0300

    Mise en place de "onDeleteItem" dans "home.dart"

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -50,7 +50,7 @@ class _HomeState extends State<Home> {
                         ToDoItem(
                           todo: todoo,
                           onToDoChanged: _handleToChange,
-                          onDeleteItem: ,
+                          onDeleteItem: _deleteToDoItem,
                         ),
                       
                     ],
