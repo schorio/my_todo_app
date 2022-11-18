67230af9779d5f5704dbe24b1af5f98df020037a
schorio <schorioignace@gmail.com>
2022-11-18 21:48:34 +0300

    Appel du liste des taches dans "home.dart"

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+
+import '../model/todo.dart';
 import '../constants/colors.dart';
 import '../widgets/todo_item.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  Home({super.key});
+
+  final todosList = ToDo.todoList();
 
   @override
   Widget build(BuildContext context) {
