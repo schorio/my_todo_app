13497682c42495acebc65544d3d61958819bd09c
schorio <schorioignace@gmail.com>
2022-11-18 21:44:41 +0300

    Creation de liste conteneant les taches

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -1,3 +1,22 @@
 class ToDo {
-  
+  String? id;
+  String? todoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone = false,
+  });
+
+  static List<ToDo> todoList() {
+    return [
+      ToDo(id: '01', todoText: 'Lundi', isDone: true),
+      ToDo(id: '02', todoText: 'Mardi', isDone: true),
+      ToDo(id: '03', todoText: 'Mercredi'),
+      ToDo(id: '04', todoText: 'Jeudi'),
+      ToDo(id: '05', todoText: 'Vendredi'),
+    ];
+  }
+
 }
