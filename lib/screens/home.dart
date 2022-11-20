323e449b08ba5c32552a7bd14329376fd60d97dc
schorio <schorioignace@gmail.com>
2022-11-20 21:09:03 +0300

    Instancier le controller de TextField des nouveaux taches

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -87,6 +87,7 @@ class _HomeState extends State<Home> {
                       borderRadius: BorderRadius.circular(10)
                     ),
                     child: TextField(
+                      controller: _todoController,
                       decoration: InputDecoration(
                         hintText: 'Ajouter une nouvelle tache',
                         border: InputBorder.none
